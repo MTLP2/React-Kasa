b3beb608e86b3db481f82f0d5afa212c12d066e0
Mathéo Lopes <matheo.lps91@gmail.com>
2023-12-14 21:01:05 +0100

    ✨ Link surligné si actif

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../assets/style/style.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Nav({logo}) {
   return (
     <nav>
         <Link to={"/"}><img src={logo} alt="" /> </Link>
         <ul>
-            <li><Link className='Link' to="/">ACCUEIL</Link></li>
-            <li><Link className='Link' to="/Apropos">A PROPOS</Link></li>
+            <li><NavLink className={({isActive, isPending}) => isActive ? "linkactive" : isPending ? "Link" : "Link"} to="/">ACCUEIL</NavLink></li>
+            <li><NavLink className={({isActive, isPending}) => isActive ? "linkactive" : isPending ? "Link" : "Link"} to="/Apropos">A PROPOS</NavLink></li>
+
         </ul>
     </nav>
   )
