c91734bf813265f29ba31f817e08f515a3ed8278
Mathéo Lopes <matheo.lps91@gmail.com>
2023-12-25 11:37:30 +0100

    ✨ Ajout des informations dans la Page a Propos

--- src/Component/Apropos.jsx
+++ src/Component/Apropos.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Boximg from './Boximg'
 import Dropbox from './Dropbox'
+import Info from '../assets/Info.json'
 
 export default function Apropos() {
+  const [info, setInfo] = useState(Info)
+
+
   return (
     <div className='majorContainer Apropos'>
       <Boximg srcImg={'/img/Image-source-2.png'}/>
       <div className='aproposInfo'>
-        <Dropbox titleDrop={'Fiabilité'}/>
-        <Dropbox titleDrop={'Respect'}/>
-        <Dropbox titleDrop={'Service'}/>
-        <Dropbox titleDrop={'Sécurité'}/>
+        <Dropbox titleDrop={'Fiabilité'} contentDrop={info.Fiabilite}/>
+        <Dropbox titleDrop={'Respect'} contentDrop={info.Respect}/>
+        <Dropbox titleDrop={'Service'} contentDrop={info.Service}/>
+        <Dropbox titleDrop={'Sécurité'} contentDrop={info.Sécurité}/>
+        
       </div>
     </div>
   )
